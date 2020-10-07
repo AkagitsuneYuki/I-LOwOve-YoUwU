462488e0f3c60d211ce073dce8a13e01c6e8e0c2
AkagitsuneYuki <michael.lovallo@gmail.com>
2020-10-06 21:45:24 -0400

    Initial commit

---
+++ Assets/Scripts/Testing/MovementChild.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class MovementChild : MonoBehaviour
+{
+
+    [SerializeField] private Transform parentTransform;
+
+    //might remove this depending on how i go about this
+    [SerializeField] private bool canMove;
+    public bool CanMove
+    {
+        get
+        {
+            return canMove;
+        }
+    }
+
+    void Start()
+    {
+        canMove = true;
+    }
+
+    void FixedUpdate()
+    {
+        MoveToParent();
+    }
+
+    private void MoveToParent()
+    {
+
+        float newX = (float)Math.Round((transform.position.x + parentTransform.position.x) / 2, 1);
+
+        if (transform.position.x != parentTransform.position.x)
+        {
+            //check is the absolute difference of my x and parent's x, subtract 0.1, then rounded to 1 decimal point
+            //when 0, we round to the nearest int. anyother number will be ignored
+            float check = (float)Math.Round(Mathf.Abs(Mathf.Abs(transform.position.x) - Mathf.Abs(parentTransform.position.x)) - 0.1f, 1);
+            if (check == 0)
+            {
+                print("rounding x to integer");
+                newX = Mathf.Round(newX);
+            }
+        }
+
+        float newY = (float)Math.Round((transform.position.y + parentTransform.position.y) / 2, 1);
+
+        if (transform.position.y != parentTransform.position.y)
+        {
+            //check is the absolute difference of my y and parent's y, subtract 0.1, then rounded to 1 decimal point
+            //when 0, we round to the nearest int. anyother number will be ignored
+            float check = (float)Math.Round(Mathf.Abs(Mathf.Abs(transform.position.y) - Mathf.Abs(parentTransform.position.y)) - 0.1f, 1);
+            if (check == 0)
+            {
+                print("rounding y to integer");
+                newY = Mathf.Round(newY);
+            }
+        }
+
+        transform.position = new Vector3(newX, newY);
+    }
+}
---
+++ Assets/Scripts/Testing/MovementParent.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementParent : MonoBehaviour
+{
+
+    [SerializeField] private Transform child;
+
+    enum MovementType
+    {
+        Primary,    //owo
+        Secondary   //uwu
+    }
+
+    [SerializeField] private MovementType movementType;
+
+    void Start()
+    {
+        float x = Mathf.FloorToInt(transform.position.x);
+        float y = Mathf.FloorToInt(transform.position.y);
+        Vector3 pos = new Vector3(x, y, 0);
+        transform.position = pos;
+    }
+
+
+    void Update()
+    {
+        if(child.position.x == transform.position.x)
+        {
+            if(child.position.y == transform.position.y)
+            {
+                Movement();
+            }
+        }
+        
+    }
+
+    private void Movement()
+    {
+        switch (movementType)
+        {
+            //owo
+            case MovementType.Primary:
+                //up
+                if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    transform.position += Vector3.up;
+                    break;
+                }
+                //down
+                if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    transform.position -= Vector3.up;
+                    break;
+                }
+                //left
+                if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    transform.position += Vector3.left;
+                    break;
+                }
+                //right
+                if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    transform.position -= Vector3.left;
+                    break;
+                }
+                break;
+            //uwu
+            case MovementType.Secondary:
+                if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    transform.position -= Vector3.up;
+                    break;
+                }
+                if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    transform.position += Vector3.up;
+                    break;
+                }
+                if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    transform.position -= Vector3.left;
+                    break;
+                }
+                if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    transform.position += Vector3.left;
+                    break;
+                }
+                break;
+        }
+    }
+
+}
